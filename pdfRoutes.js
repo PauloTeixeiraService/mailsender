e363cffbe36f64813baa4740e6c31a6bb7a32566
Miguel Silva <msilva@bidirco.com>
2024-03-31 03:05:34 +0100

    ...

--- pdfRoutes.js
+++ pdfRoutes.js
@@ -6,6 +6,8 @@ const multer  = require("multer")
 const storage = multer.memoryStorage() // or use disk storage
 const upload = multer({ storage })
 
+pdfRoute.get("/", (req, res) => { res.send("Express on Vercel"); })
+
 pdfRoute.post('/sendPdf', upload.single("file"), sendPdf);
 
 // pdfRoute.post('/sendPdf',sendPdf) //sent pdf to mail 
